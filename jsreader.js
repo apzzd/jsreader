62c4f645c0ca4fc1941dca6ebca2facfb0688162
Rose Pezzuti Dyer <pezzutidyer@uchicago.edu>
2020-07-01 16:14:42 -0500

    finish up these confusing templates!

--- jsreader.js
+++ jsreader.js
@@ -13,8 +13,8 @@ var feeds = new JSReader({
 	this.JSReader = function(){
 		// TODO: move urls to local storage
 		var defaults = {
-	      containingElement: 'body',
-	      itemTemplate: '<article><h3><a href="" target="_blank" rel="noopener"></a></h3><p></p></article>',
+	      containerSelector: 'body',
+	      itemTemplate: '<article><h3><a target="_blank" rel="noopener"></a></h3><p></p></article>',
 	      rssUrls: ['https://codepen.io/picks/feed/','https://www.sans.org/tip-of-the-day/rss']
 	    }
 
@@ -22,7 +22,7 @@ var feeds = new JSReader({
 	    	this.options = extendDefaults(defaults, arguments[0]);
 	    }
 
-	    fetchfeeds();
+	    fetchfeeds(this);
 	}
 
 	JSReader.prototype.open = function() {
@@ -40,12 +40,11 @@ var feeds = new JSReader({
 		return source;
 	}
 
-	function fetchfeeds(){
-		console.log(this);
-		options.rssUrls.forEach(url => fetchfeed(`${url}`));
+	function fetchfeeds(obj){
+		obj.options.rssUrls.forEach(url => fetchfeed(obj, `${url}`));
 	}
 
-	function fetchfeed(url){
+	function fetchfeed(obj, url){
 		fetch(url)
 			.then(response => response.text())
 			.then(str => new window.DOMParser().parseFromString(str, "text/xml"))
@@ -53,17 +52,19 @@ var feeds = new JSReader({
 				// TODO: use template for channel
 				const items = data.querySelectorAll("item");
 				items.forEach(el => {
-					var title = `${el.querySelector('title').innerHTML}`;
-					title = clean(title);
+					var title = clean(el.querySelector('title').innerHTML);
+					var description = clean(el.querySelector('description').innerHTML);
+					var href = el.querySelector('link').innerHTML;
 
-					var description = `${el.querySelector('description').innerHTML}`;
-					description = clean(description);
+					var template = document.createElement('template');
+					template.innerHTML = obj.options.itemTemplate;
 
-					node = document.importNode(feeditemtemplate, true);
+					node = document.importNode(template.content, true);
 					node.querySelector('a').text = title;
 					node.querySelector('a').href = `${el.querySelector('link').innerHTML}`;
 					node.querySelector('p').innerHTML = description;
-					document.body.appendChild(node);
+					document.querySelector(obj.options.containerSelector).appendChild(node);
+					
 				});
 		 	});
 	}
